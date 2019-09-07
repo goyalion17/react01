f98e476697d171cd69ce77099e697d8806d3a9e8
goyalion17 <goyalion17@gmail.com>
2019-09-07 20:10:24 +0300

    Create active Navbar

--- src/App.js
+++ src/App.js
@@ -4,14 +4,27 @@ import "./App.css";
 import Header from "./componets/Header/Header";
 import Profile from "./componets/Profile/Profile";
 import Navbar from "./componets/Nav/Nav";
+import Dialogs from "./componets/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+import News from './componets/News/News';
+import Music from './componets/Music/Music';
+import Settings from './componets/Settings/Settings';
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <Profile />
-    </div>
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div class="app-wrapper-content">
+          <Route path='/profile' component={Profile} />
+          <Route path='/dialogs' component={Dialogs} />
+          <Route path='/news' component={News} />
+          <Route path='/music' component={Music} />
+          <Route path='/settings' component={Settings} />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 };
 
--- src/componets/Nav/Nav.jsx
+++ src/componets/Nav/Nav.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import s from './Nav.module.css';
+import {NavLink} from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className={s.nav}>
        <ul>
-         <li><a href="" className={`${s.item} ${s.active}`}>Profile</a></li>
-         <li><a href="" className={s.item}>Messages</a></li>
-         <li><a href="" className={s.item}>News</a></li>
-         <li><a href="" className={s.item}>Music</a></li>
-         <li><a href="" className={s.item}>Settings</a></li>
+         <li><NavLink to="/profile" activeClassName={s.activeLink} className={s.item}>Profile</NavLink></li>
+         <li><NavLink to="/dialogs" activeClassName={s.activeLink} className={s.item}>Messages</NavLink></li>
+         <li><NavLink to="/news" activeClassName={s.activeLink} className={s.item}>News</NavLink></li>
+         <li><NavLink to="/music" activeClassName={s.activeLink} className={s.item}>Music</NavLink></li>
+         <li><NavLink to="/settings" activeClassName={s.activeLink} className={s.item}>Settings</NavLink></li>
        </ul>
      </nav>
   );
