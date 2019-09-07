4006fc5feafcb18081c2aa965a1e20cc3e57c116
goyalion17 <goyalion17@gmail.com>
2019-09-07 21:43:37 +0300

    Add dialogs

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
         <Navbar />
         <div class="app-wrapper-content">
           <Route path='/profile' component={Profile} />
-          <Route path='/dialogs' component={Dialogs} />
+          <Route exact path='/dialogs' component={Dialogs} />
           <Route path='/news' component={News} />
           <Route path='/music' component={Music} />
           <Route path='/settings' component={Settings} />
--- src/componets/Dialogs/Dialogs.jsx
+++ src/componets/Dialogs/Dialogs.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import s from "./Dialogs.module.css";
+import {NavLink} from 'react-router-dom';
 
 const Dialogs = props => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + ' ' + s.active}><NavLink to="/dialogs/1">Ирина</NavLink></div>
+        <div className={s.dialog}><NavLink to="/dialogs/2">Ваня</NavLink></div>
+        <div className={s.dialog}><NavLink to="/dialogs/3">Людмила</NavLink></div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>What's up</div>
+        <div className={s.message}>Yo</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
